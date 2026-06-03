5ce37a0ebdab4c971eef02b6afbd7888ff43e42b
iCkS1NGH <i.iconic.email@gmail.com>
2026-06-03 17:57:43 +0530

    implemented solutions for given assignments in student-app

--- Day02/student-app/README.md
+++ Day02/student-app/README.md
@@ -1,8 +1,16 @@
 # StudentApp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.2.13.
-
+```
 This project has custom student information done by editing template project "student-app".
   1. I learned to introduce infromation on webpage by manippulation files in ./src/aap.html and ./src/app/app.ts
   2. Installed npm packages.
   3. Ran the serve command for the preview.
+```
+```
+Implemented solution for questions provided as assignment. One can find solutions marked using comment in files:
+- ./src/app/app.html
+- ./src/app/app.ts
+And assignment questions is provided in file:
+- ./homeworkQuestions.txt
+```
--- Day02/student-app/src/app/app.ts
+++ Day02/student-app/src/app/app.ts
@@ -1,16 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+//Q2. Create one variable using 'let' and one using 'const'. Explain the difference in comments.
+  // `let` can be reassigned later if the value needs to change.
+  let cityName: string = 'Chennai';
+  // `const` cannot be reassigned after it is initialized.
+  const courseName: string = 'Angular Fundamentals';
+ //end of solution for Q2 
+
+ //Q4. Create an interface named Student with id, name, branch, and age properties.
+ export interface Student {
+  id: number;
+  name: string;
+  branch: string;
+  age: number;
+}
+// end of solution for Q4
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('student-app');
-  studentName: String = "icksingh";
+  //Q1-Q3. Create variables: name, age, city, and course using appropriate TypeScript data types && Create variables of type string, number, and boolean.
+  name: string = 'icksingh';
   age: number = 20;
-  isStudent: boolean = true;
+  city: string = "Kolkata";
+  course: string = "Frontend with Angular"; //end of solution of Q1
+  isStudent: boolean = true;  //end of solution for Q3
 }
-
-
